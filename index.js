162c1f8fd988c6e781056acef8cae078139ce14c
caosdp-rs <caosdp@gmail.com>
2022-01-25 01:24:33 -0300

    versao partials

--- index.js
+++ index.js
@@ -1,18 +1,22 @@
 const express = require("express");
 const app = express();
-
+const bodyParser = require("body-parser");
 app.set('view engine','ejs');
 app.use(express.static('public'));
-app.get("/",function(req,res){
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
 
+app.get("/",function(req,res){
     res.render("index");
 });
-
-app.get("/perguntar",function(req,res){
-
+app.get("/perguntar",(req,res)=> {
     res.render("perguntar");
-});
-
+})
+app.post("/salvarpergunta", (req,res) =>{
+    var titulo = req.body.titulo;
+    var descricao = req.body.descricao;
+    res.send("formulario recebido -" + titulo + "-" + descricao);
+})
 app.listen(3000, function(erro){
     if(erro){
         console.log("erro");
