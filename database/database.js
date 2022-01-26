0645ed45ac9df72ca79ab9f13ba0e84505da705a
caosdp-rs <caosdp@gmail.com>
2022-01-26 00:02:42 -0300

    Nova versão com banco de dados

---
+++ database/database.js
@@ -0,0 +1,6 @@
+const Sequelize = require('sequelize');
+const connection = new Sequelize('perguntas','root','',{
+    host: 'localhost',
+    dialect: 'mysql'
+});
+module.exports = connection;
--- index.js
+++ index.js
@@ -1,12 +1,26 @@
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
+const connection = require('./database/database');
+const PerguntaModel = require("./database/Pergunta");
+//database
+connection
+    .authenticate()
+    .then(() => {
+        console.log("conexão feita com sucesso!")
+    })
+    .catch((msgErro) => {
+        console.log(msgErro);
+    })
 app.set('view engine','ejs');
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
 app.get("/",function(req,res){
+    PerguntaModel.findAll().then(perguntas => {
+        console.log(perguntas);
+    });
     res.render("index");
 });
 app.get("/perguntar",(req,res)=> {
@@ -15,7 +29,12 @@ app.get("/perguntar",(req,res)=> {
 app.post("/salvarpergunta", (req,res) =>{
     var titulo = req.body.titulo;
     var descricao = req.body.descricao;
-    res.send("formulario recebido -" + titulo + "-" + descricao);
+    PerguntaModel.create({
+        titulo:titulo,
+        descricao:descricao
+    }).then(()=>{
+        res.redirect("/");
+    });
 })
 app.listen(3000, function(erro){
     if(erro){
