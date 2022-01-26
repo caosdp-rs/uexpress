e61cb3841dc20853b268ae561cfe381696f5016a
caosdp-rs <caosdp@gmail.com>
2022-01-26 01:27:24 -0300

    Banco Postgresql

--- database/database.js
+++ database/database.js
@@ -1,6 +1,12 @@
 const Sequelize = require('sequelize');
-const connection = new Sequelize('perguntas','root','',{
+/*const connection = new Sequelize('perguntas','root','',{
     host: 'localhost',
     dialect: 'mysql'
+});*/
+
+const connection = new Sequelize('d32u4tnv0uvmad','lldjdqimiipqro','fcc3491701967c8dfe3e1b5107dfb6f97c0572ecbc7467200db2895a364a93a1',{
+    host: 'ec2-3-227-149-67.compute-1.amazonaws.com',
+    port:'5432',
+    dialect: 'postgres'
 });
 module.exports = connection;
--- index.js
+++ index.js
@@ -1,26 +1,26 @@
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
-//const connection = require('./database/database');
-//const PerguntaModel = require("./database/Pergunta");
+const connection = require('./database/database');
+const PerguntaModel = require("./database/Pergunta");
 //database
-/*connection
+connection
     .authenticate()
     .then(() => {
         console.log("conexão feita com sucesso!")
     })
     .catch((msgErro) => {
         console.log(msgErro);
-    })*/
+    })
 app.set('view engine','ejs');
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
 app.get("/",function(req,res){
-    //PerguntaModel.findAll().then(perguntas => {
-    //    console.log(perguntas);
-    //});
+    PerguntaModel.findAll().then(perguntas => {
+        console.log(perguntas);
+    });
     res.render("index");
 });
 app.get("/perguntar",(req,res)=> {
@@ -29,12 +29,12 @@ app.get("/perguntar",(req,res)=> {
 app.post("/salvarpergunta", (req,res) =>{
     var titulo = req.body.titulo;
     var descricao = req.body.descricao;
-   /* PerguntaModel.create({
+    PerguntaModel.create({
         titulo:titulo,
         descricao:descricao
     }).then(()=>{
         res.redirect("/");
-    });*/
+    });
     res.send("formulario recebido -" + titulo + "-" + descricao);
 })
 app.listen(process.env.PORT || 3000, function(erro){
